97f87e09219d2d7025cad6efac63d0ee4ad381f8
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-05-31 01:34:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,14 +9,15 @@ outer_results = []
 row_index = 0
  while row_index < src.count do
     element_index = 0
-    smallest_string_element = ""
+    smallest_string_element = []
+    lowest_value = 100
     while element_index < src[row_index].count do
-        if src[row_index][element_index].length > longest_string_element.length
-      longest_string_element = src[row_index][element_index]
+        if src[row_index][element_index]< lowest_value
+      lowest_value = src[row_index][element_index]
       end
       element_index += 1
     end
-    outer_results << smallest_string_element
+    outer_results << lowest_value
     row_index += 1
   end
 outer_results
